2ca03c042f8fc0e04c8cb0feb715f519dad35c6c
Mariana <marianagg0412@gmail.com>
2024-01-24 11:50:08 -0500

    POR FAVOR

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Otra cosa");
+        System.out.println("Julio deje la bareta o invite a loco");
+        System.out.println("AAAAAAAAA");
+    }
+}
