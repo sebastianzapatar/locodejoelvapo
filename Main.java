9a712b5ec06b45104c93cf03c36244ce284f11a0
santy-estrada <santiago.estrada6@eia.edu.co>
2024-01-24 11:52:06 -0500

    Conflicto starter

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Otra cosa");
         System.out.println("Julio deje la bareta o invite a loco");
+        System.out.println("Comida fast");
         System.out.println("AAAAAAAAA");
     }
 }
